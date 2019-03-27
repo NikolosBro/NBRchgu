e44bf30d3e6e78f963a490f96281937ef3b15a0c
NikolosBro <nik422.2011@mail.ru>
2019-03-27 19:04:27 +0300

    test1

---
+++ 1.py
@@ -0,0 +1,10 @@
+import re
+x=str(input('Введите чилсо на примере (-87.21E-5)'))
+z=re.compile(r'(^[-+][0-9]+[.][0-9]+[E][-+][0-9]+$)')
+z1=z.search(x)
+if z1==None:
+    print('False')
+elif x==z1.group():
+    print('true')
+else:
+    print('False')
